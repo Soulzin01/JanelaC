491744dd16779aad22828f9c0e3f5d996690db5c
Soulzin01 <112558190+Soulzin01@users.noreply.github.com>
2023-06-24 21:07:00 -0300

    commit: save

--- biblioteca.c
+++ biblioteca.c
@@ -1,10 +1,42 @@
 #include "biblioteca.h"
 #include <stdio.h>
 
-void janela(int xInicial, int yInicial, int xFinal, int yFinal){
-    for(int i = xInicial; i <= xFinal; i++){
-        for(int j = yInicial; j <= yFinal; j++){
-            printf("-");
+void janela();
+
+void janela(int xInicial, int yInicial, int xFinal, int yFinal)
+{
+    for (int i = xInicial; i <= xFinal; i++)
+    {
+        for (int j = yInicial; j <= yFinal; j++)
+        {
+            if (i == xInicial && j == yInicial)
+            {
+                printf("SE");
+            }
+            else if (i == xInicial && j == yFinal)
+            {
+                printf("SD");
+            }
+            else if (i == xFinal && j == yInicial)
+            {
+                printf("IE");
+            }
+            else if (i == xFinal && j == yFinal)
+            {
+                printf("ID");
+            }
+            else if (i == xInicial || i == xFinal)
+            {
+                printf("=");
+            }
+            else if (j == yInicial || j == yFinal)
+            {
+                printf("|");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
     }
 }
--- main.c
+++ main.c
@@ -1,7 +1,22 @@
-#include "biblioteca.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char const *argv[])
+void gotoxy(int x, int y) { printf("\033[%d;%dH", y, x); }
+
+int main()
 {
-    janela(1, 1, 24, 80);
+    int linha = 5, coluna = 10;
+
+    system("cls"); // limpa a tela
+
+    gotoxy(coluna, linha); // vai para a posição coluna 10, linha 5
+
+    printf("%c", 204);
+
+    sleep(2); // espera 2 segundos
+
+    getchar(); // espera uma entrada do teclado
+
     return 0;
 }
