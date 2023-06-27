0b025915d601b2c0dccf7c0abaf36613b5a6fc22
Soulzin01 <112558190+Soulzin01@users.noreply.github.com>
2023-06-26 22:48:35 -0300

    commit: final adjust

--- main.c
+++ main.c
@@ -6,9 +6,10 @@
 
 int main()
 {
-    char nome[26], peso[5];
-    int idade;
-    inicializar_tela();
+    char nome[26], peso[5]; // Inicialização das variáveis que serão passadas nas funções de leitura
+    int idade; // Inicialização das variáveis que serão passadas nas funções de leitura
+    
+    inicializar_tela(); //Inicialização da tela utilizando os parâmetros que foram recebidos
 
     label(2, 2, "Nome: ");
     label(2, 3, "Idade: ");
@@ -17,8 +18,9 @@ int main()
     ler_string(nome, 7, 2);
     ler_inteiro(&idade, 8, 3);
     ler_decimal(peso, 7, 4);
-
-    gotoxy(1, 25);
+    gotoxy(2, 19);
+    printf("Resultado:");
+    gotoxy(2, 20);
     printf("Nome: %s | Idade: %d Anos | Peso: %s Kg", nome, idade, peso);
     
     
